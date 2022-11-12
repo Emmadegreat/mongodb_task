cd5d6169b53c80cd3990a3a5ede3fea342d18b17
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-11-12 09:28:51 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const connect = require("./config/database")
 const express  = require("express");
-
+import bodyParser from "body-parser";
 connect()
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/user', userRoute);
 
 app.get("/", (req, res) => {
     res.send(`welcome to mongoose db`);
